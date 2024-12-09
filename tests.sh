665af11f3abbbef0de08286784195b886de9d479
clbu <claudia.bura@numberly.com>
2024-12-09 14:40:58 +0100

    tests

--- tests.sh
+++ tests.sh
@@ -31,7 +31,7 @@ export NETBOX_AGENT__NETBOX__URL="http://$(docker compose port netbox 8080)"
 export NETBOX_AGENT__NETBOX__TOKEN='0123456789abcdef0123456789abcdef01234567'
 
 cd -
-coverage run -m pytest
+pytest
 
 cd netbox-docker
 docker compose down
